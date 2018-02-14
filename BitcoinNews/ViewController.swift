b6f4bd7bbd3fd5ee830cff98a440c5ec3c1d1cc5
Alex Shcherbakov <alexander@prolificinteractive.com>
2018-02-14 15:48:27 -0800

    Initial Commit

---
+++ BitcoinNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinNews
+//
+//  Created by Alex Shcherbakov on 2/14/18.
+//  Copyright © 2018 Oleks Shcherbakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
